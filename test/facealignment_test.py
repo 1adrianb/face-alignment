35c01f731e072cadce982caab75790fa413e0f2f
Adrian Bulat <bulat.adrian@gmail.com>
2017-09-18 17:27:34 +0100

    test tests :)

--- test/facealignment_test.py
+++ test/facealignment_test.py
@@ -5,3 +5,4 @@ class Tester(unittest.TestCase):
     @classmethod
     def setUpClass(self):
         self._fa = face_alignment.FaceAlignment(face_alignment.LandmarksType._3D, enable_cuda=False)
+    
