c33f32636df6ff3d3ff96e1ce580b22e04e572d8
Adrian Bulat <bulat.adrian@gmail.com>
2018-10-11 19:09:03 +0100

    Quick update for conda

---
+++ conda/conda_upload.sh
@@ -0,0 +1,8 @@
+PCG_NAME=face_alignment
+USER=1adrianb
+
+mkdir ~/conda-build
+conda config --set anaconda_upload no
+export CONDA_BUILD_PATH=~/conda-build
+conda build conda/
+anaconda -t $CONDA_UPLOAD_TOKEN upload -u $USER $CONDA_BUILD_PATH/$PKG-NAME-1-tar.bz2 --force
