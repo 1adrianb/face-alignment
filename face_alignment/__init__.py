a6e1a6d1d7b2cc8a13e3fb32a53de75226a3c382
Adrian Bulat <adrian@adrianbulat.com>
2020-07-31 17:18:00 +0100

    forgot yo bump version

--- face_alignment/__init__.py
+++ face_alignment/__init__.py
@@ -2,6 +2,6 @@
 
 __author__ = """Adrian Bulat"""
 __email__ = 'adrian.bulat@nottingham.ac.uk'
-__version__ = '1.0.1'
+__version__ = '1.1.0'
 
 from .api import FaceAlignment, LandmarksType, NetworkSize
