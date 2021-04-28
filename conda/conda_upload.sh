3995416443716ba77d390735366785988436995a
Adrian Bulat <adrian@adrianbulat.com>
2021-04-28 23:12:40 +0100

    version bump

--- conda/conda_upload.sh
+++
@@ -1,7 +0,0 @@
-PCG_NAME=face_alignment
-USER=1adrianb
-
-mkdir ~/conda-build
-conda config --set anaconda_upload no
-conda build conda/
-anaconda -t $CONDA_UPLOAD_TOKEN upload -u $USER /home/travis/miniconda/envs/test-environment/conda-bld/noarch/face_alignment-1.3.3-py_1.tar.bz2 --force
--- face_alignment/__init__.py
+++ face_alignment/__init__.py
@@ -2,6 +2,6 @@
 
 __author__ = """Adrian Bulat"""
 __email__ = 'adrian@adrianbulat.com'
-__version__ = '1.3.3'
+__version__ = '1.3.4'
 
 from .api import FaceAlignment, LandmarksType, NetworkSize
